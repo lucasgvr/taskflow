bf2a237cc756aa16b3196da2adbf7093f0a1bfbd
Lucas <lucas.voriarocha@gmail.com>
2024-08-06 16:45:49 -0300

    Atualizar notas ok

--- src/pages/TaskEditPage/index.jsx
+++ src/pages/TaskEditPage/index.jsx
@@ -7,11 +7,18 @@ import { useEmployees } from '../../hooks/useEmployees'
 import { doc, updateDoc, getDoc } from 'firebase/firestore'
 import { db } from '../../services/firebase'
 
-import { Box } from '@chakra-ui/react'
+import { Button } from '../../components/Button'
+
+import { Box, Textarea, Heading, VStack, Text, HStack, IconButton } from '@chakra-ui/react'
 
 import { Header } from '../../components/TaskEdit/Header'
 import { Main } from '../../components/TaskEdit/Main'
 import { Aside } from '../../components/TaskEdit/Aside'
+
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+import { DeleteIcon } from '@chakra-ui/icons';
 
 export function TaskEditPage() {
     const [task, setTask] = useState([])
@@ -20,6 +27,10 @@ export function TaskEditPage() {
     const [newDeadline, setNewDeadline] = useState("")
     const [newStatus, setNewStatus] = useState("")
     const [newAssign, setNewAssign] = useState("")
+    // eslint-disable-next-line
+    const [newNotes, setNewNotes] = useState("")
+    const [newNote, setNewNote] = useState("")
+
 
     const params = useParams()
     const taskId = params.id
@@ -44,10 +55,11 @@ export function TaskEditPage() {
         setNewDeadline(task.deadline)
         setNewStatus(task.status)
         setNewAssign(task.assign)
+        setNewNotes(task.notes)
         // eslint-disable-next-line
     }, [])
 
-    const updateTask = async (id, newDescription, newDeadline, newStatus, newAssign) => {
+    const updateTask = async (id, newDescription, newDeadline, newStatus, newAssign, updatedNotes) => {
         const taskDoc = doc(db, "tasks", id)
 
         let assignRef
@@ -62,23 +74,103 @@ export function TaskEditPage() {
             description: newDescription || task.description, 
             deadline: newDeadline || task.deadline, 
             status: newStatus || task.status,
-            assign: assignRef || task.assign
+            assign: assignRef || task.assign,
+            notes: updatedNotes || task.notes
         }
-
-        console.log(newFields)
 
         await updateDoc(taskDoc, newFields)
 
-        navigate("/home")
+        toast.success('Tarefa atualizada com sucesso!')
+
+        setTimeout(() => {
+            navigate('/home')
+        }, 5000)
     }
 
+    const addNote = () => {
+        const createdAt = new Date().toISOString()
+
+        const updatedNotes = [
+            ...(task.notes || []), 
+            { 
+                description: newNote,
+                createdBy: 'Lucas Rocha',
+                createdAt: createdAt
+            }
+        ]        
+        setTask({ ...task, notes: updatedNotes });
+        toast.success('Observação adicionada com sucesso!')
+        setNewNote("");
+    };
+
+    const handleDeleteNote = (noteIndex) => {
+        const updatedNotes = task.notes.filter((_, index) => index !== noteIndex);
+        setTask({ ...task, notes: updatedNotes });
+        toast.success('Observação removida com sucesso!');
+    };
+
     return (
-        <Box as='div' bgColor='#F2F0F5' height='100vh' fontWeight='500' color='#FCFDFF' border='none'>
+        <Box as='div' fontWeight='500' color='#FCFDFF' border='none'>
             <Header taskId={taskId} />
-            <Box as='div' width='min(1440px, 90vw)' margin='0 auto' display='flex'>
+            <Box as='div' width='min(1440px, 90vw)' margin='0 auto' display='flex' justifyContent='center'>
                 <Main taskId={taskId} setNewDescription={setNewDescription} setNewDeadline={setNewDeadline} setNewStatus={setNewStatus} setNewAssign={setNewAssign} departments={departments} employees={employees} />
                 <Aside updateTask={updateTask} taskId={taskId} newDescription={newDescription} newDeadline={newDeadline} newStatus={newStatus} newAssign={newAssign} /> 
             </Box>
+
+            <Box as='div' width='min(900px, 90vw)' margin='4rem auto' display='flex' gap='1rem' alignItems='center' justifyContent='center'>
+                <Textarea
+                    placeholder="Adicionar observações..."
+                    backgroundColor='#fff'
+                    color='#5A5A66'
+                    borderRadius='0.313rem'
+                    resize='none'
+                    value={newNote}
+                    onChange={(e) => setNewNote(e.target.value)}
+                />  
+                <Button onClick={addNote}>Adicionar</Button>
+            </Box>
+            <Box as='div' width='min(900px, 90vw)' margin='1rem auto'>
+                <Heading size="md" mb="1rem" color='#5A5A66'>Observações</Heading>
+                <VStack spacing={4} align="start" width='100%'>
+                        {task && task.notes && task.notes.length > 0 ? task.notes.map((note, index) => (
+                        <Box key={index} p={4} width='100%' borderRadius="0.313rem" border='1px solid #E1E3E6'>
+                            <HStack justifyContent="space-between" width='100%'>
+                                <VStack align="start" width='100%'>
+                                    <Text color='#5A5A66'>{note.description}</Text>
+                                    <Box display='flex' justifyContent='space-between' width='100%'>
+                                        <Text color='#5A5A66' fontSize="sm">Criado por: {note.createdBy}</Text>
+                                        <Text color='#5A5A66' fontSize="sm">Data de criação: {new Date(note.createdAt).toLocaleString()}</Text>
+                                    </Box>
+                                </VStack>
+                                <IconButton
+                                    icon={<DeleteIcon />}
+                                    aria-label="Delete Note"
+                                    onClick={() => handleDeleteNote(index)}
+                                    bgColor='var(--orange)'
+                                    size='sm'
+                                    transition='0.25s'
+                                    _hover={{ filter: 'brightness(0.9)' }}
+                                />
+                            </HStack>
+                        </Box>
+                    )) : (
+                        <Text color='#5A5A66'>Não há Observações para esta tarefa</Text>
+                    )}
+                </VStack>
+            </Box>
+
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={true}
+                closeOnClick
+                rtl={false}
+                draggable
+                theme="light"
+                pauseOnFocusLoss={false}
+                pauseOnHover={false}
+            />
         </Box>
     )
 }
